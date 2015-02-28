6d8f4f0ab25a121a959c2b2613357343db392cb7
Kerem Gunes <qeremy@gmail.com>
2015-02-28 20:08:02 +0200

    Finalize methods.

--- Oppa/Configuration.php
+++ Oppa/Configuration.php
@@ -5,63 +5,63 @@ class Configuration
 {
     protected $options = [];
 
-    public function __construct(array $options = []) {
+    final public function __construct(array $options = []) {
         if (!empty($options)) {
             $this->sets($options);
         }
     }
 
-    public function __set($key, $value) {
+    final public function __set($key, $value) {
         return $this->set($key, $value);
     }
 
-    public function __get($key) {
+    final public function __get($key) {
         return $this->get($key);
     }
 
-    public function __isset($key) {
+    final public function __isset($key) {
         return $this->offsetExists($key);
     }
 
-    public function __unset($key) {
+    final public function __unset($key) {
         return $this->offsetUnset($key);
     }
 
-    public function sets($options) {
+    final public function sets($options) {
         foreach ($options as $key => $value) {
             $this->set($key, $value);
         }
     }
 
-    public function set($key, $value) {
+    final public function set($key, $value) {
         $this->options[$key] = $value;
         return $this;
     }
 
-    public function get($key, $defaultValue = null) {
+    final public function get($key, $defaultValue = null) {
         if ($this->offsetExists($key)) {
             return $this->options[$key];
         }
         return $defaultValue;
     }
 
-    public function offsetSet($key, $value) {
+    final public function offsetSet($key, $value) {
         return $this->set($key, $value);
     }
 
-    public function offsetGet($key) {
+    final public function offsetGet($key) {
         return $this->get($key);
     }
 
-    public function offsetExists($key) {
+    final public function offsetExists($key) {
         return isset($this->options[$key]);
     }
 
-    public function offsetUnset($key) {
+    final public function offsetUnset($key) {
         unset($this->options[$key]);
     }
 
-    public function toArray() {
+    final public function toArray() {
         return $this->options;
     }
 }
