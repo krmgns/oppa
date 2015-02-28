51f4ad8fb9f2f463a6e775180135c9e9c97fd9d9
Kerem Gunes <qeremy@gmail.com>
2015-03-01 00:15:51 +0200

    Add docs.

--- Oppa/Factory.php
+++ Oppa/Factory.php
@@ -1,8 +1,44 @@
-<?php namespace Oppa;
+<?php
+/**
+ * Copyright (c) 2015 Kerem Gunes
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+namespace Oppa;
+
+/**
+ * @package    Oppa
+ * @object     Oppa\Factory
+ * @implements Oppa\Shablon\FactoryInterface
+ * @version    v1.0
+ * @author     Kerem Gunes <qeremy@gmail>
+ */
 final class Factory
     implements \Oppa\Shablon\FactoryInterface
 {
+    /**
+     * Build as internal Oppa object.
+     *
+     * @param  string     $className
+     * @param  array|null $arguments
+     * @return object
+     */
     final public static function build($className, array $arguments = null) {
         if ($className[0] != '\\') {
             $className = '\\'. $className;
