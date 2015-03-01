3ab565751463b0421d12e299a03a90899b2c2c81
Kerem Gunes <qeremy@gmail.com>
2015-03-01 22:32:12 +0200

    Add docs.

--- Oppa/Database/Query/Result/Mysqli.php
+++ Oppa/Database/Query/Result/Mysqli.php
@@ -1,11 +1,47 @@
-<?php namespace Oppa\Database\Query\Result;
+<?php
+/**
+ * Copyright (c) 2015 Kerem Gunes
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Oppa\Database\Query\Result;
 
 use \Oppa\Exception\Database as Exception;
 
+/**
+ * @package    Oppa
+ * @subpackage Oppa\Database\Query\Result
+ * @object     Oppa\Database\Query\Result\Mysqli
+ * @uses       Oppa\Exception\Database
+ * @extends    Oppa\Database\Query\Result
+ * @version    v1.0
+ * @author     Kerem Gunes <qeremy@gmail>
+ */
 final class Mysqli
     extends \Oppa\Database\Query\Result
 {
 
+    /**
+     * Free resource.
+     *
+     * @return void
+     */
     final public function free() {
         if ($this->result instanceof \mysqli_result) {
             $this->result->free();
@@ -13,6 +49,11 @@ final class Mysqli
         }
     }
 
+    /**
+     * Reset Result vars.
+     *
+     * @return void
+     */
     final public function reset() {
         $this->id = [];
         $this->rowsCount = 0;
@@ -20,12 +61,28 @@ final class Mysqli
         $this->data = [];
     }
 
+    /**
+     * Process result.
+     *
+     * If query action contains "select", then process returned result.
+     * If query action contains "update/delete", etc then process affected result.
+     *
+     * @param  object/resource $link
+     * @param  object/resource $result
+     * @param  integer         $limit
+     * @param  integer         $fetchType
+     * @throws Oppa\Exception\Database\ArgumentException
+     * @return self
+     */
     final public function process($link, $result, $limit = null, $fetchType = null) {
+        // check link
         if (!$link instanceof \mysqli) {
-            throw new Exception\ArgumentException('Process link must be instanceof mysqli!');
+            throw new Exception\ArgumentException(
+                'Process link must be instanceof mysqli!');
         }
 
         $i = 0;
+        // if result contains result object
         if ($result instanceof \mysqli_result && $result->num_rows) {
             if ($limit == null) {
                 $limit = PHP_INT_MAX;
@@ -63,6 +120,7 @@ final class Mysqli
             }
         }
 
+        // free result
         $this->free();
 
         // dirty ways to detect last insert id for multiple inserts
