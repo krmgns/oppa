f0c9a656a04e96577e995690ed7632772bf80d4c
Kerem Gunes <qeremy@gmail.com>
2015-03-01 02:48:25 +0200

    Add docs.

--- Oppa/Shablon/Database/DatabaseInterface.php
+++ Oppa/Shablon/Database/DatabaseInterface.php
@@ -1,10 +1,66 @@
-<?php namespace Oppa\Shablon\Database;
+<?php
+/**
+ * Copyright (c) 2015 Kerem Gunes
+ *    <http://qeremy.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+namespace Oppa\Shablon\Database;
+
+/**
+ * @package    Oppa
+ * @subpackage Oppa\Shablon\Database
+ * @object     Oppa\Shablon\Database\DatabaseInterface
+ * @version    v1.0
+ * @author     Kerem Gunes <qeremy@gmail>
+ */
 interface DatabaseInterface
 {
+    /**
+     * Connect action pattern.
+     *
+     * @param string $host
+     */
     public function connect($host = null);
+
+    /**
+     * Disconnect action pattern.
+     *
+     * @param string $host
+     */
     public function disconnect($host = null);
+
+    /**
+     * Is connected action pattern.
+     *
+     * @param string $host
+     */
     public function isConnected($host = null);
+
+    /**
+     * Get connection action pattern.
+     *
+     * @param string $host
+     */
     public function getConnection($host = null);
+
+    /**
+     * Info action pattern.
+     */
     public function info();
 }
