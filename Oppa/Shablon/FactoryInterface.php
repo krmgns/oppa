7282b489ba99511a2149a08eb06bd48950b715e5
Kerem Gunes <qeremy@gmail.com>
2015-03-01 02:06:02 +0200

    Add docs.

--- Oppa/Shablon/FactoryInterface.php
+++ Oppa/Shablon/FactoryInterface.php
@@ -1,5 +1,12 @@
 <?php namespace Oppa\Shablon;
 
+/**
+ * @package    Oppa
+ * @subpackage Oppa\Shablon
+ * @object     Oppa\Shablon\FactoryInterface
+ * @version    v1.0
+ * @author     Kerem Gunes <qeremy@gmail>
+ */
 interface FactoryInterface
 {
     public static function build($className, array $arguments = null);
