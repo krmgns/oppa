dcc273ec4bf277a49d2800e2e33991c051afd697
Kerem Gunes <qeremy@gmail.com>
2015-11-10 02:14:44 +0200

    update version.

--- Oppa/Database/Query/Builder.php
+++ Oppa/Database/Query/Builder.php
@@ -31,7 +31,7 @@ use \Oppa\Exception\Database as Exception;
  * @subpackage Oppa\Database\Query
  * @object     Oppa\Database\Query\Builder
  * @uses       Oppa\Helper, Oppa\Exception\Database, Oppa\Database\Connector\Connection
- * @version    v1.16
+ * @version    v1.17
  * @author     Kerem Gunes <qeremy@gmail>
  */
 final class Builder
