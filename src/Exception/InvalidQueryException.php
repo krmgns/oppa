3d764458b23da97bcd17911e84765fd572578b61
Kerem Güneş <k-gun@mail.com>
2017-01-11 02:25:11 +0300

    Add Oppa\Exception\InvalidQueryException object.

---
+++ src/Exception/InvalidQueryException.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright (c) 2015 Kerem Güneş
+ *    <k-gun@mail.com>
+ *
+ * GNU General Public License v3.0
+ *    <http://www.gnu.org/licenses/gpl-3.0.txt>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace Oppa\Exception;
+
+/**
+ * @package    Oppa
+ * @subpackage Oppa\Exception
+ * @object     Oppa\Exception\InvalidQueryException
+ * @author     Kerem Güneş <k-gun@mail.com>
+ */
+final class InvalidQueryException extends \InvalidArgumentException
+{}
