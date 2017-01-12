da0dfff4cf99176030a5fcc7a4044a3748ddd787
Kerem Güneş <k-gun@mail.com>
2017-01-12 05:49:46 +0300

    Tests..

--- test/gets.php
+++ test/gets.php
@@ -10,8 +10,8 @@ $cfg = [
         'username' => 'test',       'password' => '********',
         'charset'  => 'utf8',       'timezone' => '+00:00',
     ],
-    'fetch_type' => User::class,
-    'fetch_limit' => 1,
+    // 'fetch_type' => User::class,
+    // 'fetch_limit' => 1,
     'map_result' => true,
     'map_result_bool' => true,
 ];
@@ -24,7 +24,7 @@ $agent = $db->getLink()->getAgent();
 class User {}
 
 $result = $agent->query("select * from users");
-// $result = $agent->query("select * from users", null, null, User::class);
+// $result = $agent->query("select * from users", null, 1, User::class);
 prd($result->getData());
 
 
