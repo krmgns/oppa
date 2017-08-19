54eb600e68e538d4032270f2ed90ea19a222a655
Kerem Güneş <k-gun@mail.com>
2017-08-20 02:24:25 +0300

    Tests..

--- test/gets.php
+++ test/gets.php
@@ -21,12 +21,25 @@ $db->connect();
 
 $agent = $db->getLink()->getAgent();
 
-class User {}
+class User {
+    // public function __construct()
+    // {
+    //     // die(__class__);
+    // }
+    // public function __set($name,$value)
+    // {
+    //     pre($name);
+    //     $this->{$name}=$value;
+    // }
+}
 
 // $result = $agent->query("select * from users");
+// pre($result->toClass(User::class));
 // $result = $agent->query("select * from users", null, 1, User::class);
+// pre($result);
+
 $result = $agent->get("select * from users", null, User::class);
-$result = $agent->getAll("select * from users", null, User::class);
+// $result = $agent->getAll("select * from users", null, User::class);
 pre($result);
 
 
